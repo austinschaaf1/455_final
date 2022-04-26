af03b16d3f67c6ef548823e7aeb26c4765321f84
austinschaaf <austin.schaaf@und.edu>
2022-04-26 16:27:03 -0500

    finishing touches

--- info_silo/pages/searchKeywordPage.py
+++ info_silo/pages/searchKeywordPage.py
@@ -50,86 +50,115 @@ class SEARCH_KEYWORD(QDialog):  # INDEX = 5
         
 
     def addKeywordToFav(self):
-        # Determine if keyword exists or not if it does add it to users interests and give conformation message
-        ###SQL needed
-        ## if select is not null
-        ##  SELECT * FROM keyword WHERE (keyword_name LIKE %userSearch%);
-        ##  INSERT INTO user_interest (user_interest_id, search_word_id, interest_type) VALUES (userID, keywordID, interestType);
+        ##adds a user selected keyword to the database if it is found
+
+        ##determine if keyword is in the database
         addKeyWordInput = self.enterKeywordInput.text()
         sql = "SELECT * FROM keyword WHERE (keyword_name LIKE %s)"
         val = (addKeyWordInput,)
         self.mySQL.execute(sql, val)
         keywordInfo = self.mySQL.fetchall()
+
         if len(keywordInfo) == 0:
             ##Keyword not found in database
-            print("miss")
+            message = addKeyWordInput + " not in Database try Request"
+            self.success_label.setText(message)
+            self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
             pass
         else:
             ###keyword found in database
             ########need to change to have User ID
             userID = 5 ######Chris Change
+
+            ###Determine if keyword exists inside the users interests so its not readded
             sql = "SELECT * FROM user_interest WHERE user_id=%s AND keyword_key=%s"
             val = (userID,keywordInfo[0][0],)
             self.mySQL.execute(sql, val)
             userInterestMatch = self.mySQL.fetchall()
-            if len(userInterestMatch) == 0:
-                #keyword not in user_interests
+
+            if len(userInterestMatch) == 0: #keyword not in user_interests
+                
+                ##Insert the keyword into user interests
                 sql = "INSERT INTO user_interest (user_id, keyword_key, interest_type) VALUES (%s, %s, %s)"
                 val = (userID,keywordInfo[0][0],"KEY",)
                 self.mySQL.execute(sql, val)
                 self.db.commit()
-                #need to add success message
-            else:
-                #need to add fail message
-                pass
-            
-            
-            
-        
+                message = addKeyWordInput + " added to your interests!"
+                self.success_label.setText(message)
+                self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
 
+            else: #keyword is already in users interests
+                message = addKeyWordInput + " is already in your interests!"
+                self.success_label.setText(message)
+                self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
         
-
     def requestKeyword(self):
-        # Determine if keyword exists if it does not add it to keyword managers list and give conformation message
-        ###SQL needed examples
-        # If select is null
-        ##  SELECT * FROM keyword WHERE (keyword_name LIKE %userSearch%);
-        ##  INSERT INTO pending_keyword (keyword, date_requested) VALUES (pendKeyword, currentDate);
-        requestKeyWordInput = self.requestKeywordInput.text()
-        sql = "SELECT * FROM keyword WHERE (keyword_name LIKE %s)"
-        val = (requestKeyWordInput,)
+        ####Adds the keyword to the keyword manager page
+        ########need to change to have User ID
+        userID = 5 ######Chris Change
+
+        #determine if membership level is high enough
+        sql = "SELECT * FROM user_data WHERE user_id=%s"
+        val = (userID,)
         self.mySQL.execute(sql, val)
-        keywordInfo = self.mySQL.fetchall()
-        if len(keywordInfo) == 0:
-            ##Keyword not found in database Send it to the keyword manager
+        userinfo = self.mySQL.fetchall()
+        
+        if(userinfo[0][3] == 5):
+            ##determine if keyword is inside the keyword table
             requestKeyWordInput = self.requestKeywordInput.text()
-            sql = "SELECT * FROM pending_keyword WHERE (keyword LIKE %s)"
+            sql = "SELECT * FROM keyword WHERE (keyword_name LIKE %s)"
             val = (requestKeyWordInput,)
             self.mySQL.execute(sql, val)
-            keywordMangerInfo = self.mySQL.fetchall()
-            if len(keywordMangerInfo) == 0:
-                sql = "INSERT INTO pending_keyword (keyword, date_requested) VALUES (%s, %s)"
-                val = (requestKeyWordInput,date.today(),)
+            keywordInfo = self.mySQL.fetchall()
+
+            if len(keywordInfo) == 0: ##Keyword not found in database Send it to the keyword manager
+                
+                ###Determine if the keyword is not already in the keyword manager table
+                requestKeyWordInput = self.requestKeywordInput.text()
+                sql = "SELECT * FROM pending_keyword WHERE (keyword LIKE %s)"
+                val = (requestKeyWordInput,)
                 self.mySQL.execute(sql, val)
-                self.db.commit()
-            
-            pass
-        else:
-            ###keyword found in database
-            ########need to change to have User ID
-            userID = 5 ######Chris Change
-            sql = "SELECT * FROM user_interest WHERE user_id=%s AND keyword_key=%s"
-            val = (userID,keywordInfo[0][0],)
-            self.mySQL.execute(sql, val)
-            userInterestMatch = self.mySQL.fetchall()
-            if len(userInterestMatch) == 0:
-                #keyword not in user_interests
-                sql = "INSERT INTO user_interest (user_id, keyword_key, interest_type) VALUES (%s, %s, %s)"
-                val = (userID,keywordInfo[0][0],"KEY",)
-                self.mySQL.execute(sql, val)
-                self.db.commit()
-                #need to add success message
+                keywordMangerInfo = self.mySQL.fetchall()
+
+                if len(keywordMangerInfo) == 0: ##### keyword not in the keyword manager table
+                    sql = "INSERT INTO pending_keyword (keyword, date_requested) VALUES (%s, %s)"
+                    val = (requestKeyWordInput,date.today(),)
+                    self.mySQL.execute(sql, val)
+                    self.db.commit()
+
+                ##Success message
+                message = requestKeyWordInput + " has been sent to keyword manager!"
+                self.success_label.setText(message)
+                self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
             else:
-                #need to add fail message
-                pass
-        pass
+                ###keyword found in database
+
+
+                ###Determine if the keyword is in the users interests already
+                sql = "SELECT * FROM user_interest WHERE user_id=%s AND keyword_key=%s"
+                val = (userID,keywordInfo[0][0],)
+                self.mySQL.execute(sql, val)
+                userInterestMatch = self.mySQL.fetchall()
+
+                if len(userInterestMatch) == 0: #keyword not in user_interests
+                    
+                    ######Add the keyword to the users interests
+                    sql = "INSERT INTO user_interest (user_id, keyword_key, interest_type) VALUES (%s, %s, %s)"
+                    val = (userID,keywordInfo[0][0],"KEY",)
+                    self.mySQL.execute(sql, val)
+                    self.db.commit()
+
+                    ###success message
+                    message = requestKeyWordInput + " has been added to your interests!"
+                    self.success_label.setText(message)
+                    self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
+                    
+                else: ###user already has requested keyword
+                    
+                    message = requestKeyWordInput + " is already in your interests!"
+                    self.success_label.setText(message)
+                    self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
+        else: ###not high enough membership Level
+            message = "Please purchase paid version to request keyword"
+            self.success_label.setText(message)
+            self.success_label.setStyleSheet("color:blue;background: none;font-size:25px;font-weight: bold;")
